9dc2614052c980a231e27ee612a97d53c29a6ef5
Rafael <rfviolato@gmail.com>
2020-05-12 21:39:21 +0200

    feat: add base styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import styled from '@emotion/styled';
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import { motion } from "framer-motion";
 
 const Root = styled.div`
   display: flex;
@@ -9,12 +10,52 @@ const Root = styled.div`
   color: #fff;
   background: cornflowerblue;
   font-size: 24px;
+  font-family: sans-serif;
 `;
 
+const SearchFieldWrapper = styled(motion.div)`
+  width: 400px;
+  height: 75px;
+  border-radius: 7px;
+  background-color: #fff;
+`;
+
+const SearchField = styled.input`
+  border: 0;
+  outline: 0;
+  width: 100%;
+  height: 75px;
+  font-size: 30px;
+  padding: 0 16px;
+  background-color: transparent;
+
+  &::placeholder {
+    font-size: 0.8em;
+    opacity: 0.6;
+    font-style: italic;
+  }
+`;
+
+const variants = {
+  collapsed: {
+    height: 75,
+  },
+  expanded: {
+    height: 300,
+  },
+};
+
 function App() {
+  const [isExpanded, setExpanded] = useState(false);
+
   return (
-    <Root className="App">
-      This is my app!
+    <Root onClick={() => setExpanded(!isExpanded)}>
+      <SearchFieldWrapper
+        animate={isExpanded ? "expanded" : "collapsed"}
+        variants={variants}
+      >
+        <SearchField type="text" placeholder="type something..." />
+      </SearchFieldWrapper>
     </Root>
   );
 }
