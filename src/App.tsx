f0ff54f7f9483f02febc8665cc72379e4b6e2fb0
Rafael <rfviolato@gmail.com>
2020-05-13 21:13:04 +0200

    feat: add breathing effect for loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import styled from "@emotion/styled";
 import { motion } from "framer-motion";
+import debounce from "lodash.debounce";
+
+const DIMENSIONS = {
+  INPUT: {
+    INITIAL_WIDTH: 400,
+    INITIAL_HEIGHT: 75,
+  },
+};
 
 const Root = styled.div`
   display: flex;
@@ -13,21 +21,30 @@ const Root = styled.div`
   font-family: sans-serif;
 `;
 
+const Content = styled.div`
+  position: relative;
+`;
+
 const SearchFieldWrapper = styled(motion.div)`
-  width: 400px;
-  height: 75px;
-  border-radius: 7px;
+  position: absolute;
+  z-index: 2;
+  top: 0;
+  left: 0;
+  width: ${DIMENSIONS.INPUT.INITIAL_WIDTH}px;
+  height: ${DIMENSIONS.INPUT.INITIAL_HEIGHT}px;
   background-color: #fff;
 `;
 
 const SearchField = styled.input`
+  position: relative;
   border: 0;
   outline: 0;
-  width: 100%;
-  height: 75px;
+  width: ${DIMENSIONS.INPUT.INITIAL_WIDTH}px;
+  height: ${DIMENSIONS.INPUT.INITIAL_HEIGHT}px;
   font-size: 30px;
   padding: 0 16px;
   background-color: transparent;
+  z-index: 2;
 
   &::placeholder {
     font-size: 0.8em;
@@ -36,26 +53,55 @@ const SearchField = styled.input`
   }
 `;
 
-const variants = {
-  collapsed: {
-    height: 75,
-  },
-  expanded: {
-    height: 300,
-  },
-};
+let timeout: any;
+function createAnimateAttribute(isLoading: boolean) {
+  if (isLoading) {
+    return { scale: [1, 1.03, 1] };
+  }
+
+  return {};
+}
+
+function createTransitionAttribute(isLoading: boolean) {
+  if (isLoading) {
+    return {
+      times: [0, 0.5, 1],
+      duration: 2,
+      loop: true,
+      yoyo: Infinity,
+      ease: "easeInOut",
+    };
+  }
+}
 
 function App() {
-  const [isExpanded, setExpanded] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const onSearch = () => {
+    setLoading(true);
+
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(() => setLoading(false), 1500);
+  };
+  const debouncedOnChange = useMemo(() => debounce(onSearch, 400), []);
+
+  console.log({ isLoading });
 
   return (
-    <Root onClick={() => setExpanded(!isExpanded)}>
-      <SearchFieldWrapper
-        animate={isExpanded ? "expanded" : "collapsed"}
-        variants={variants}
-      >
-        <SearchField type="text" placeholder="type something..." />
-      </SearchFieldWrapper>
+    <Root>
+      <Content>
+        <SearchFieldWrapper
+          animate={createAnimateAttribute(isLoading)}
+          transition={createTransitionAttribute(isLoading)}
+        />
+        <SearchField
+          type="text"
+          placeholder="type something..."
+          onChange={() => debouncedOnChange()}
+        />
+      </Content>
     </Root>
   );
 }
